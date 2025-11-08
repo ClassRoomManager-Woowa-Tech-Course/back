61c9ed28a9f96349219b099dd6e8d4fd0eaedb9b
juntae6942 <juntae6942@o.cnu.ac.kr>
2025-11-08 22:28:59 +0900

    refactor: repository annotation 추가

--- src/main/java/com/classroom/manager/user/domain/repository/AdminRepository.java
+++ src/main/java/com/classroom/manager/user/domain/repository/AdminRepository.java
@@ -2,6 +2,8 @@ package com.classroom.manager.user.domain.repository;
 
 import com.classroom.manager.user.domain.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AdminRepository extends JpaRepository<Admin, String> {
 }
--- src/main/java/com/classroom/manager/user/domain/repository/MemberRepository.java
+++ src/main/java/com/classroom/manager/user/domain/repository/MemberRepository.java
@@ -2,6 +2,8 @@ package com.classroom.manager.user.domain.repository;
 
 import com.classroom.manager.user.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, String> {
 }
